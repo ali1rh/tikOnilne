c4c264ef555d493385957ffcbc4d8382abf9a773
AliReza Heidari <h118ar@gmail.com>
2023-05-17 17:07:07 +0400

    remove map

--- pages/contactPage/secContact/googleMap/Map.js
+++ pages/contactPage/secContact/googleMap/Map.js
@@ -1,23 +1,26 @@
 import React from "react";
-import { useGoogleMaps} from "react-hook-google-maps";
-import styles from "../../../../styles/sass/pages/contactPage/secContact/secContact.module.scss"
+// import { useGoogleMaps} from "react-hook-google-maps";
+import styles from "../../../../styles/sass/pages/contactPage/secContact/secContact.module.scss";
 
 const GoogleMap = () => {
+//   const { ref } = useGoogleMaps("AIzaSyBCiJRvCAHw_XBuuv3R_xwQyzTAwryG530", {
+//     center: { lat: 40.244014, lng: 44.574557 },
+//     zoom: 15,
+//   });
 
-    
-    const { ref } = useGoogleMaps(
-        "AIzaSyBCiJRvCAHw_XBuuv3R_xwQyzTAwryG530",
-        {
-            center: { lat:  40.244014, lng: 44.574557 },
-            zoom: 15,
-            
-        },
-    );
+  return (
+    <>
+      <div>map not loaded</div>
+      {/* <div
+        className={styles.Map}
+        ref={ref}
+        style={{
+          width: "45.666666666666664vw",
+          height: "32.552083333333336vw",
+        }}
+      ></div> */}
+    </>
+  );
+};
 
-    return(<div className={styles.Map} ref={ref} style={{ width: '45.666666666666664vw', height: '32.552083333333336vw'}} >
-        
-    </div>)
-}
-
-
-export default GoogleMap
+export default GoogleMap;
--- pages/howToGetToMall/microBus/Map.js
+++ pages/howToGetToMall/microBus/Map.js
@@ -1,23 +1,27 @@
 import React from "react";
-import { useGoogleMaps} from "react-hook-google-maps";
-import styles from "../../../styles/sass/pages/contactPage/secContact/secContact.module.scss"
+// import { useGoogleMaps} from "react-hook-google-maps";
+import styles from "../../../styles/sass/pages/contactPage/secContact/secContact.module.scss";
 
 const GoogleMap = () => {
+  // const { ref } = useGoogleMaps(
+  //     "AIzaSyBCiJRvCAHw_XBuuv3R_xwQyzTAwryG530",
+  //     {
+  //         center: { lat:  40.244014, lng: 44.574557 },
+  //         zoom: 15,
 
-    
-    const { ref } = useGoogleMaps(
-        "AIzaSyBCiJRvCAHw_XBuuv3R_xwQyzTAwryG530",
-        {
-            center: { lat:  40.244014, lng: 44.574557 },
-            zoom: 15,
-            
-        },
-    );
+  //     },
+  // );
 
-    return(<div className={styles.Map} ref={ref} style={{ width: '89vw', height: '40vw'}} >
-        
-    </div>)
-}
+  return (
+    <>
+      <div>map not loaded</div>
+      {/* <div
+        className={styles.Map}
+        ref={ref}
+        style={{ width: "89vw", height: "40vw" }}
+      ></div> */}
+    </>
+  );
+};
 
-
-export default GoogleMap
+export default GoogleMap;
